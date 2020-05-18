6793513b18956685067051eb7b1c0ffb3432ab8b
edper <edpermail@yahoo.com>
2020-05-19 00:39:22 +1100

    got about page and titles done

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,6 +3,15 @@
 	import CovidChart from '../components/CovidChart.svelte';
 	import TableContainer from '../components/TableContainer.svelte';
 </script>
+<svelte:head>
+    <title>Covid 19 US Tracker</title>
+</svelte:head>
+
+<div class="section header">
+	<div class="container">
+		<h1>Covid 19 US</h1>
+	</div>
+</div>
 
 <CovidStat/>
 <CovidChart/>
